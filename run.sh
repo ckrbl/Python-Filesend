bb5c9ec3b350ab7b43eaabaab4cabb3fcc882433
Chris K <99300381+ckrbl@users.noreply.github.com>
2024-05-16 02:49:29 +0000

    Rename pyfileupload to app.py, allow invoking the file directly

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash                                                                           
-                                                                                              
-export FLASK_APP=pyfileupload.py                                                              
-flask run --host=0.0.0.0                                                                      
-
